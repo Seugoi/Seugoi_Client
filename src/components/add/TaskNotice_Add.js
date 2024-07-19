e934551c29fd0c08e5bad5b58128b87487e06c65
bhark065 <w2216@e-mirim.hs.kr>
2024-07-19 22:11:28 +0900

    Style: 선택 시 색 변경

--- src/components/add/TaskNotice_Add.js
+++ src/components/add/TaskNotice_Add.js
@@ -18,11 +18,11 @@ function TaskNotice_Add() {
             <TitleBar title="과제 / 공지 추가" link="" />
             <div className={styles['div-select']}>
                 <div className={styles['select']}>
-                    <div className={styles['task-add']} onClick={() => select('task')}>
+                    <div className={styles['task-add']} onClick={() => select('task')} style={{ backgroundColor: addSelect === 'task' ? '#252831' : null }}>
                         과제 추가
                     </div>
 
-                    <div className={styles['notice-add']} onClick={() => select('notice')}>
+                    <div className={styles['notice-add']} onClick={() => select('notice')} style={{ backgroundColor: addSelect === 'notice' ? '#252831' : null }}>
                         공지 추가
                     </div>
                 </div>
