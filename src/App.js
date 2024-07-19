30c370dcd53ed31ad71de06484692fb49a314984
bhark065 <w2216@e-mirim.hs.kr>
2024-07-19 20:32:36 +0900

    Feat: 과제/공지 추가 구현

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 
+import TaskNoticeAdd from './pages/TaskNotice_Add';
+
 function App() {
   return (
     <Router>
       <Routes>
-
+        <Route path='/add' element={<TaskNoticeAdd />} />
       </Routes>
     </Router>
   );
