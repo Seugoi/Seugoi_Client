31d65c60d983fc7ae1b2b6d5fe8936c63e7af579
bhark065 <w2216@e-mirim.hs.kr>
2024-07-20 15:37:45 +0900

    Feat: 과제 컴포넌트 구현

---
+++ src/components/add/task/TaskList.js
@@ -0,0 +1,11 @@
+import '../../../styles/common/Style.css';
+
+import TaskItem from './TaskItem';
+
+function TaskList() {
+    return (
+        <TaskItem />
+    )
+}
+
+export default TaskList;
--- src/components/study/TaskView.js
+++ src/components/study/TaskView.js
@@ -2,11 +2,13 @@ import '../../styles/common/Style.css';
 import styles from '../../styles/study/TaskView.module.css';
 
 import NoticeList from '../add/notice/NoticeList';
+import TaskList from '../add/task/TaskList';
 
 function TaskView() {
     return (
         <div className={styles['container']}>
             <NoticeList />
+            <TaskList />
         </div>
     )
 }
