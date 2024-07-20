5f7659a9372d4f879f5708537fe7f36704216fb2
bhark065 <w2216@e-mirim.hs.kr>
2024-07-20 19:52:48 +0900

    Feat: 프로필 컴포넌트

---
+++ src/components/study/Profile.js
@@ -0,0 +1,15 @@
+import '../../styles/common/Style.css';
+import styles from '../../styles/study/Profile.module.css';
+
+function Profile() {
+    return (
+        <div className={styles['container']}>
+            <div className={styles['img']}>
+                <img src='https://3ba1f5b2.rocketcdn.me/wp-content/uploads/2012/01/Difference-Between-Example-and-Sample.jpg' />
+            </div>
+            <p>최보람</p>
+        </div>
+    )
+}
+
+export default Profile;
