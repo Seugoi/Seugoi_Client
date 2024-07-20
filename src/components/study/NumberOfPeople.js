634356c690e7ac6fadf2f52b56c93a1d91c8598a
bhark065 <w2216@e-mirim.hs.kr>
2024-07-20 19:52:35 +0900

    Feat: 모집인원 컴포넌트

---
+++ src/components/study/NumberOfPeople.js
@@ -0,0 +1,14 @@
+import { Icon } from '@iconify/react';
+import '../../styles/common/Style.css';
+import styles from '../../styles/study/NumberOfPeople.module.css';
+
+function NumberOfPeople() {
+    return (
+        <div className={styles['container']}>
+            <Icon icon="fluent:person-48-filled" fontSize="15" color='#52DD63' />
+            <p>모집인원 : 19/10</p>
+        </div>
+    )
+}
+
+export default NumberOfPeople;
