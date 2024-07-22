fd74b86aa8a6eedd6323cabe604feb7fde0d187a
rkdbs <mw2207@naver.com>
2024-07-22 09:14:08 +0900

    Feat: 3초 후 join 화면 전환

--- src/pages/Splash.js
+++ src/pages/Splash.js
@@ -1,6 +1,19 @@
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 import styles from '../styles/splash/Splash.module.css';
 
 function SplashPage() {
+  const navigate = useNavigate();  
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigate('/join');
+    }, 5000);
+  
+    return () => clearTimeout(timer);
+  }, [navigate]);
+
   return (
     <div className={styles['container']}>
       <img
