8aef703336f3dab42f4e559707b52c0008742d1f
rkdbs <mw2207@naver.com>
2024-07-22 19:20:14 +0900

    Feat: 카카오 로그인 ui

--- src/components/join/Button.js
+++ src/components/join/Button.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import styles from '../../styles/join/Button.module.css';
 
-function Button() {
+const Button = () => {
+  const kakaoAuthUrl = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${process.env.REACT_APP_REST_API_KEY}&redirect_uri=${process.env.REACT_APP_REDIRECT_URI}`;
+
+  const handleLogin = () => {
+    window.location.href = kakaoAuthUrl;
+  }
+
   return (
-    <div className={styles['login-button']}>
-      <img
-        src={process.env.PUBLIC_URL + '/images/kakaotalk.png'}
-        alt="kakao"
-      />
-      <p>카카오로 시작하기</p>
+    <div className={styles['login-button']} onClick={handleLogin}>
+        <img
+          src='/images/kakaotalk.png'
+          alt="kakao"
+        />
+        <p>카카오로 시작하기</p>
     </div>
   );
 }
