7b5ca98b487730e9816bec353b754b852a71c5d3
bhark065 <w2216@e-mirim.hs.kr>
2024-07-23 13:27:10 +0900

    Style: 카카오 로그인 성공 후 home 화면으로 이동

--- src/App.js
+++ src/App.js
@@ -4,14 +4,20 @@ import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import StudyDetail from './pages/StudyDetail';
 import TaskNoticeAdd from './pages/TaskNoticeAdd';
 import MyStudy from './pages/MyStudy';
+import LikeStudy from './pages/LikeStudy';
 import Splash from './pages/Splash';
 import Home from './pages/Home';
 import Join from './pages/Join';
+import Info from './components/join/Info';
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path='/' element={<Splash />} />
+        <Route path='/join' element={<Join />} />
+        <Route path='/seugoi_kakaoOAuth' element={<Info />} />
+
         <Route path='/home' element={<Home />} />
         
         <Route path='/study/1' element={<StudyDetail />} />
@@ -20,9 +26,7 @@ function App() {
 
         <Route path='/my_study' element={<MyStudy />} />
 
-        <Route path='/' element={<Splash />} />
-
-        <Route path='/join' element={<Join />} />
+        <Route path='/like_study' element={<LikeStudy />} />
       </Routes>
     </Router>
   );
--- src/components/join/Info.js
+++ src/components/join/Info.js
@@ -1,14 +1,26 @@
 import { useEffect, useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
 const Info = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+
   const [userId, setUserId] = useState(null);
 
   useEffect(() => {
     const fetchUserInfo = async () => {
+      const params = new URLSearchParams(location.search);
+      const code = params.get('code');
       try {
-        const response = await axios.get(`http://localhost:3000/seugoi_kakaoOAuth`);
-        setUserId(response.data);
+        const response = await axios.post(`${process.env.REACT_APP_HOST}/users/kakao_oauth`, { code });
+        if (response.status === 200) {
+          setUserId(response.data);
+          console.log(response.data);
+          navigate("/home");
+        } else {
+          console.log("카카오 로그인 실패", response.status);
+        }
       } catch (error) {
         console.error('Error: ', error);
       }
