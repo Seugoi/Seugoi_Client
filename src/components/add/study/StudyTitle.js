391a8ffbe540cfc1da79a8bb66053819b40e89a2
해원 <haewon1106@gmail.com>
2024-07-29 19:38:29 +0900

    Docs: 클래스 추가

--- src/components/add/study/StudyTitle.js
+++ src/components/add/study/StudyTitle.js
@@ -3,7 +3,7 @@ import styles from '../../../styles/add/study/StudyTitle.module.css';
 function StudyTitle({ text }) {
     return (
         <div className={styles['container']}>
-            <p dangerouslySetInnerHTML={{ __html: text }}></p>
+            <p dangerouslySetInnerHTML={{ __html: text }} className={styles['title']}></p>
         </div>
     );
 }
