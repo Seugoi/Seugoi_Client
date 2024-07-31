3b29bc5aec01c77108080f42c5dc13220db59a4b
bhark065 <w2216@e-mirim.hs.kr>
2024-07-31 14:29:08 +0900

    Feat: 만든/가입한 스터디 조회 서버 연결

--- src/components/my_study/JoinStudyList.js
+++ src/components/my_study/JoinStudyList.js
@@ -1,13 +1,37 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import '../../styles/common/Style.css';
 import styles from '../../styles/my_study/JoinStudyList.module.css';
 
 import StudyItem from './StudyItem';
 
 function JoinStudyList() {
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        async function joinStudy() {
+            try {
+                const response = await axios.get(`${process.env.REACT_APP_HOST}/users/1/join`);
+                if (response.status === 200) {
+                    console.log("가입한 스터디 조회 성공");
+                    setData(response.data);
+                } else {
+                    console.log("가입한 스터디 조회 실패", response.status);
+                }
+            } catch(err) {
+                console.error(err);
+            }
+        }
+        joinStudy();
+    }, [])
+
     return (
         <div className={styles['container']}>
-            <StudyItem />
-            <StudyItem />
+            {
+                data && data.map((item, index) => (
+                    <StudyItem data={item.study} key={index} />
+                ))
+            }
         </div>
     )
 }
