a45125fc53ebf2335095e3ca164fb14fdfba55c6
bhark065 <w2216@e-mirim.hs.kr>
2024-08-03 21:58:45 +0900

    Feat: 찜하기 토글 기능 구현 및 item 클릭 시 세부화면으로 이동하는 기능 구현

--- src/components/study/like_study/LikeStudyItem.js
+++ src/components/study/like_study/LikeStudyItem.js
@@ -1,24 +1,54 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Icon } from '@iconify/react';
+import axios from 'axios';
 import '../../../styles/common/Style.css';
 import styles from '../../../styles/study/like_study/LikeStudyItem.module.css';
-import { useState } from 'react';
 
-function LikeStudyItem() {
+function LikeStudyItem({ data }) {
+    const navigate = useNavigate();
     const [clickedIcon, setClickedIcon] = useState('iconamoon:bookmark-fill');
 
-    const clickIcon = () => {
+    async function clickLike(study_id) {
+        try {
+            const request = await axios.post(`${process.env.REACT_APP_HOST}/study/like`, {
+                user_id: 1,
+                study_id: study_id
+            });
+
+            if(request.status === 200) {
+                console.log(request.data.message);
+            } else {
+                console.log("찜하기 실패", request.status);
+            }
+        } catch(err) {
+            console.error(err);
+        }
+    }
+
+    const clickIcon = (e) => {
+        e.stopPropagation();
+        clickLike(data.id);
         setClickedIcon(prevIcon => 
             prevIcon === 'iconamoon:bookmark-fill' 
             ? 'iconamoon:bookmark-light' 
             : 'iconamoon:bookmark-fill'
         );
     }
+    
+    const click = (id) => {
+        navigate(`/study/${id}`);
+    }
 
     return (
-        <div className={styles['container']}>
+        <div className={styles['container']} onClick={() => click(data.id)}>
             <div className={styles['inner-container']}>
                 <div className={styles['img']}>
-                    <img src='https://3ba1f5b2.rocketcdn.me/wp-content/uploads/2012/01/Difference-Between-Example-and-Sample.jpg' />
+                    {data && data.image ? (
+                        <img src={data.image} />
+                    ) : (
+                        <img src="https://3ba1f5b2.rocketcdn.me/wp-content/uploads/2012/01/Difference-Between-Example-and-Sample.jpg" />
+                    )}
                 </div>
 
                 <div className={styles['content']}>
@@ -31,8 +61,12 @@ function LikeStudyItem() {
                         />
                     </div>
                     <div className={styles['info']}>
-                        <p>java를 꽉 java</p>
-                        <p>#java #개발 #코딩</p>
+                        {data && data.name ? (
+                            <p>{data.name}</p>
+                        ) : (
+                            <p>이름없음</p>
+                        )}
+                        <p>#{data.category.tag1} #{data.category.tag2}</p>
                     </div>
                 </div>
             </div>
