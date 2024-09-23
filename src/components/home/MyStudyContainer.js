7f133c1a91232242531b45503d5bff5b4b2eb7ad
bhark065 <w2216@e-mirim.hs.kr>
2024-09-23 09:31:52 +0900

    Feat: 데이터가 없을 시 메시지 보여주기

--- src/components/home/MyStudyContainer.js
+++ src/components/home/MyStudyContainer.js
@@ -7,9 +7,11 @@ export default function MyStudyContainer({ data }) {
     return (
         <div className={styles['item-container']}>
             {
-                data && data.map((item, index) => (
+                data ? data.map((item, index) => (
                     <MyStudyItem data={item.study} key={index} />
-                ))
+                )) : (
+                    <p style={{ color: 'white', marginBottom: '5%' }}>현재 진행중인 스터디가 존재하지 않습니다.</p>
+                )
             }
         </div>
     );
